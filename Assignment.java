cf9f47655e8dbeb9662a04caef2d617b0b127765
Pranam Rai <111623224+Pra17rai@users.noreply.github.com>
2023-02-16 08:17:04 +0545

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,8 @@
+public class Pranam{
+    public static void main(String[] args)
+    {
+        System.out.println("Name of organization: LIBRARY MANAGEMENT SYSTEM\n\n");
+        System.out.println("Address:\n----State: KTM\n----Zip Code: 44600\n----Country: Nepal\n");
+        System.out.println("Contact Information:\n----Mobile Number: 9849134487\n----Email: Pra17rai@gmail.com");
+    }
+}
---
+++ Display name.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Test{
+    public static void main(String[] args)
+    {
+        Scanner input =new Scanner (System.in);
+        System.out.println("My name is Pranam Rai");
+        System.out.println("Enter two numbers: ");
+        int a=input.nextInt();
+        int b=input.nextInt();
+        System.out.println("The sum of two numbers is: ",a+b);
+        input.close();
+        
+
+    }
+}
